18b9455f0318fe069c598c5b8f3a8d6cc98018fb
Alexandre Charuel <alex.charuel@gmail.com>
2013-10-15 15:35:20 +0200

    Mon premier Commit

---
+++ _debut.inc.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Festival</title>
+<meta http-equiv="Content-Language" content="fr">
+<meta charset="utf-8">
+<link href="css/bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex8-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.php">M2L</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex8-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Accueil</a></li>
+      <li><a href="listeEtablissements.php">Gestion établissements</a></li>
+      <li><a href="consultationAttributions.php">Attributions chambres</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+<p class="text-left"><img src="images/logo470px.png" width="100" height="80" /></p>
+     <p class="text-center"><h1> Festival Folklores du Monde <br>
+      H&eacute;bergement des groupes<br>&nbsp;</h1></p>
+
+
+
+
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+include "_debut.inc.php";
+
+echo " 
+<br> 
+
+<table>
+   <tr>  
+      <td>
+         Cette application web permet de gérer l'hébergement des groupes de musique 
+         durant le festival Folklores du Monde.
+      </td>
+   </tr>
+   <tr>
+      <td>&nbsp;
+      </td>
+   </tr>
+   <tr>
+      <td>
+          Elle offre les services suivants :
+      </td>
+   </tr>
+   <tr>
+      <td>&nbsp;
+      </td>
+   </tr>
+   <tr>
+      <td>
+      <ul>
+         <li>Gérer les établissements (caractéristiques et capacités d'accueil) acceptant d'héberger les groupes de musiciens.
+         <p>
+	      </p>
+         <li>Consulter, r&eacute;aliser ou modifier les attributions des chambres aux groupes dans les établissements.
+      </ul>
+      </td>
+   </tr>
+</table>";
+
+?>
